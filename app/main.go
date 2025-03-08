42843da212d4c67456ef8185510de5a95dd239e2
Abdul Rehman <ar618211@gmail.com>
2025-03-08 17:35:00 +0500

    stage 5: count sql

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	// Available if you need it!
 	// "github.com/xwb1989/sqlparser"
 )
@@ -17,21 +18,21 @@ func main() {
 	databaseFilePath := os.Args[1]
 	command := os.Args[2]
 
+	databaseFile, err := os.Open(databaseFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if command[0] != '.' {
+		executeSQL(databaseFile, command)
+		return
+	}
+
 	switch command {
 	case ".dbinfo":
-		databaseFile, err := os.Open(databaseFilePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-
 		dbinfo(databaseFile)
 
 	case ".tables":
-		databaseFile, err := os.Open(databaseFilePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-
 		tables(databaseFile)
 
 	default:
@@ -77,4 +78,41 @@ func tables(dbFile *os.File) {
 			flag = true
 		}
 	}
+}
+
+func executeSQL(databaseFile *os.File, command string) {
+	// count command
+	splitCommand := strings.Split(command, " ")
+	lastWord := splitCommand[len(splitCommand)-1]
+	if lastWord[len(lastWord)-1] == ';' {
+		lastWord = lastWord[:len(lastWord)-1]
+	}
+
+	tableName := lastWord
+	headers := parseDBHeaders(databaseFile)
+
+	pageHeaders := parsePageHeaders(databaseFile)
+
+	found := false
+	tableCell := SchemaCell{}
+
+	for _, cellAddress := range pageHeaders.CellAddresses {
+		databaseFile.Seek(int64(cellAddress), 0)
+		cell := parseCell(databaseFile)
+		if cell.Type == "table" && cell.TableName == tableName {
+			tableCell = cell
+			found = true
+		}
+	}
+	if !found {
+		fmt.Println("Table not found")
+		os.Exit(1)
+	}
+
+	offset := (uint16(tableCell.RootPage) - 1) * headers.PageSize
+
+	databaseFile.Seek(int64(offset), 0)
+
+	tablePageHeaders := parsePageHeaders(databaseFile)
+	fmt.Println(tablePageHeaders.NumberOfCells)
 }
